b0c0bc4c37f802ec37fd472e347657a63150b2a4
Sergei Kochetov <sergey.bmstu@yandex.ru>
2026-02-18 21:19:59 +0100

    Initial Commit

---
+++ Grit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grit
+//
+//  Created by Sergei Kochetov on 18/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grit/GritApp.swift
@@ -0,0 +1,17 @@
+//
+//  GritApp.swift
+//  Grit
+//
+//  Created by Sergei Kochetov on 18/2/26.
+//
+
+import SwiftUI
+
+@main
+struct GritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
